03774eafb5afad39a55c7075ed5bca818efee7e8
yasir-z <ysairch@gmail.com>
2026-07-15 08:08:14 +0000

    update: upated tests

--- product-service/app.py
+++ product-service/app.py
@@ -38,4 +38,4 @@ def get_product(product_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=5002) # nosec B104
--- user-service/app.py
+++ user-service/app.py
@@ -37,4 +37,4 @@ def get_user(user_id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5001) # nosec B104
